7c0f0cbb55e0bc2552dc71c80021473858ac8dcd
cgrdavies <cgrdavies@gmail.com>
2015-07-09 01:23:31 -0400

    remove files

--- app/db/dbReader.js
+++
@@ -1,9 +0,0 @@
-function getLine(lineNumber, callback) {
-	console.log("reading from db")
-	global.db.get(lineNumber, function(err, line) {
-		callback(err, line)
-	})
-}
-
-module.exports = getLine
-
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Installs dependencies; Assumes Node.js and npm are available. 
-
-echo "Please note: this script assumes Node.js and npm are available."
-
-npm install
